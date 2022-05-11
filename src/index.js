279e88588f87dd09bf9fb1293a62cca88aab1bf5
Paul M. Summitt <psummitt@gmail.com>
2022-05-11 18:12:54 -0500

    update metadata

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d')
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Player {
+    
+}
